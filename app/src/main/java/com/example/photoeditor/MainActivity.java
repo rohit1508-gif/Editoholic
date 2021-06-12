0bbeb1407bcf6bab2745b1607c31f00355338193
ROHIT AGRAWAL <64243388+rohit1508-gif@users.noreply.github.com>
2021-06-12 13:30:59 +0530

    SplashScreen

---
+++ app/src/main/java/com/example/photoeditor/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.photoeditor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+    }
+}
